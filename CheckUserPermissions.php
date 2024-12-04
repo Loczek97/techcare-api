47fbaf5f5089f6f104c4949ca4b67d99c509948e
Michał Szymański <szymanski.m698@gmail.com>
2024-12-04 12:34:28 +0100

    wylaczone tymczasowo sprawdzanie permisji uzytkownika

--- CheckUserPermissions.php
+++ CheckUserPermissions.php
@@ -6,15 +6,19 @@ function CheckUserPermission()
 
     // $input = json_decode(file_get_contents('php://input'), true);
 
-    // // $sql = "SELECT p.permission_name 
-    // //         FROM permissions p 
-    // //         LEFT JOIN user_permissions up ON p.permission_id = up.permission_id 
-    // //         WHERE up.user_id = :user_id;";
+    // echo json_decode($input['user_id']);
 
-    // // $result = $db->fetchAll($sql, [":user_id" => $input['user_id']]);
+    // $sql = "SELECT p.permission_name 
+    //         FROM permissions p 
+    //         LEFT JOIN user_permissions up ON p.permission_id = up.permission_id 
+    //         WHERE up.user_id = :user_id;";
+
+    // $result = $db->fetchAll($sql, [":user_id" => $input['user_id']]);
 
 
-    // http_response_code(403);
-    // echo json_encode(['status' => 'PIERDOLE', 'message' => ]);
-    // die();
+    // if (empty($result)) {
+    //     http_response_code(401);
+    //     echo json_encode(['status' => 'error', 'message' => 'Unauthorized access']);
+    //     exit();
+    // }
 }
